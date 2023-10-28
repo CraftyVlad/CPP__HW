1ac4e7ce0e5cca237856f28a592fce10db5844de
Vladsem77 <vladsemboss@gmail.com>
2023-10-28 11:20:33 +0300

    made task 1

--- Task1&2.cpp
+++ Task1&2.cpp
@@ -1,51 +1,38 @@
 #include <iostream>
 
-using namespace std;
+class Rectangle {
+private:
+    double length;
+    double width;
 
-int main()
+public:
+    Rectangle(double len, double wid) : length(len),
+        width(wid) {}
 
-{
+    double calculateArea() {
+        return length * width;
+    }
 
-	const int time = 86400;
+    double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+};
 
-	int a;
+int main() {
 
-	cout << "Enter the time in seconds elapsed since the beginning of the day" << endl;
+    double length, width;
+    std::cout << "Input the length of the rectangler: ";
+    std::cin >> length;
+    std::cout << "Input the width of the rectangle: ";
+    std::cin >> width;
 
-	cin >> a;
+    Rectangle rectangle(length, width);
 
-	int hh = a % time / 3600;
-	int mm = a / 60 % 60;
-	int ss = a % 60;
+    double area = rectangle.calculateArea();
+    std::cout << "\nArea: " << area << std::endl;
 
-	int endhh, endmm, endss;
+    double perimeter = rectangle.calculatePerimeter();
+    std::cout << "Perimeter: " << perimeter << std::endl;
 
-	int tmp = hh * 3600 + mm * 60 + ss;
-
-	tmp = time - tmp;
-	endhh = tmp / 3600;
-	endmm = tmp / 60 - endhh * 60;
-	endss = tmp - endmm * 60 - endhh * 3600;
-
-	cout << "Now is: " << hh << " hh: " << mm << " mm: " << ss << " ss" << endl;
-
-	cout << "before midnight: " << endhh << " hh: " << endmm << " mm: " << endss << " ss" << endl << endl;
-
-	int mainNumber = 1234;
-
-	cout << "Given an integer: " << mainNumber << endl;
-	cout << "Reversed number: ";
-	cout << mainNumber % 10;
-	mainNumber /= 10;
-	cout << mainNumber % 10;
-	mainNumber /= 10;
-	cout << mainNumber % 10;
-	mainNumber /= 10;
-	cout << mainNumber % 10;
-	mainNumber /= 10;
-
-	cout << endl << endl;
-
-	return 0;
-
+    return 0;
 }
