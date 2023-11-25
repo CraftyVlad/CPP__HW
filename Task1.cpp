454d7a49360334e0f5f422b284bfe40dc6934765
Vladsem77 <vladsemboss@gmail.com>
2023-11-25 14:54:13 +0200

    completed task

--- Task1.cpp
+++ Task1.cpp
@@ -4,62 +4,65 @@ using namespace std;
 
 class Wheel {
 private:
-	double diameter;
-	string tireType;
+    double diameter;
+    string tireType;
 
 public:
-	Wheel(double diameter, string tireType) : diameter(diameter), tireType(TireType) {}
+    Wheel(double diameter, string tireType) : diameter(diameter), tireType(tireType) {}
 
-	void setDiameter(double diameter) {
-		this->diameter = diameter;
-	}
-	double getDiameter() {
-		return diameter;
-	}
+    void setDiameter(double diameter) {
+        this->diameter = diameter;
+    }
 
-	void setTireType(string tireType) {
-		this->tireType = tireType;
-	}
-	double getTireType() {
-		return tireType;
-	};
+    double getDiameter() const {
+        return diameter;
+    }
+
+    void setTireType(string tireType) {
+        this->tireType = tireType;
+    }
+
+    string getTireType() const {
+        return tireType;
+    }
+};
 
 class Car {
 private:
-	Wheel* wheel5;
+    Wheel* wheels[4];
 
 public:
-	Car(Wheel* frontLeft, Wheel* frontRight, Wheel* rearLeft, Wheel* rearRight) {
-		wheel1 = frontLeft;
-		wheel2 = frontRight;
-		wheel3 = rearLeft;
-		wheel4 = rearRight;
-	};
+    Car(Wheel* frontLeft, Wheel* frontRight, Wheel* rearLeft, Wheel* rearRight) {
+        wheels[0] = frontLeft;
+        wheels[1] = frontRight;
+        wheels[2] = rearLeft;
+        wheels[3] = rearRight;
+    }
 
-	void move() {
-		cout << "Car is moving using " << wheel1->getTireType() << " tires" << endl;
-	}
+    void move() {
+        cout << "Car is moving using " << wheels[0]->getTireType() << " tires" << endl;
+    }
 
-	void stop() {
-		cout << "Car has stopped" << endl;
-	}
+    void stop() {
+        cout << "Car has stopped" << endl;
+    }
 };
 
 int main() {
-	Wheel* frontLeft = new Wheel(18, "all-season");
-	Wheel* frontRight = new Wheel(18, "all-season");
-	Wheel* rearLeft = new Wheel(18, "all-season");
-	Wheel* rearRight = new Wheel(18, "all-season");
+    Wheel* frontLeft = new Wheel(18, "all-season");
+    Wheel* frontRight = new Wheel(18, "all-season");
+    Wheel* rearLeft = new Wheel(18, "all-season");
+    Wheel* rearRight = new Wheel(18, "all-season");
 
-	Car car(frontLeft, frontRight, rearLeft, rearRight);
+    Car car(frontLeft, frontRight, rearLeft, rearRight);
 
-	car.move();
-	car.stop();
+    car.move();
+    car.stop();
 
-	delete frontLeft;
-	delete frontRight;
-	delete rearLeft;
-	delete rearRight;
+    delete frontLeft;
+    delete frontRight;
+    delete rearLeft;
+    delete rearRight;
 
-	return 0;
+    return 0;
 }
