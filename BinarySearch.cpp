7d62dd0af9a700124548ae257011d931891ccbd7
“CraftyVlad“ <vladsemboss@gmail.com>
2023-12-31 14:53:32 +0200

    made 4 tasks

---
+++ BinarySearch.cpp
@@ -0,0 +1,26 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//int binarySearch(int arr[], int l, int r, int x) {
+//    if (r >= l) {
+//        int mid = l + (r - l) / 2;
+//        if (arr[mid] == x)
+//            return mid;
+//        if (arr[mid] > x)
+//            return binarySearch(arr, l, mid - 1, x);
+//        return binarySearch(arr, mid + 1, r, x);
+//    }
+//    return -1;
+//}
+//
+//int main() {
+//    int arr[] = { 11, 12, 22, 25, 64 };
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//    int x = 22;
+//    int result = binarySearch(arr, 0, n - 1, x);
+//    if (result == -1)
+//        cout << "Element is not present in array";
+//    else
+//        cout << "Element is present at index " << result;
+//    return 0;
+//}
---
+++ LinearSearch.cpp
@@ -0,0 +1,21 @@
+﻿//#include <iostream>
+//using namespace std;
+//
+//int linearSearch(int arr[], int n, int x) {
+//    for (int i = 0; i < n; i++)
+//        if (arr[i] == x)
+//            return i;
+//    return -1;
+//}
+//
+//int main() {
+//    int arr[] = { 64, 25, 12, 22, 11 };
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//    int x = 22;
+//    int result = linearSearch(arr, n, x);
+//    if (result == -1)
+//        cout << "Element is not present in array";
+//    else
+//        cout << "Element is present at index " << result;
+//    return 0;
+//}
