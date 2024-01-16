301158ea310e3679ebe1f047f91018ed56169128
CraftyVlad <107984683+CraftyVlad@users.noreply.github.com>
2024-01-16 15:11:34 +0200

    Update Task1.cpp

--- Task1&2.cpp
+++ Task1&2.cpp
@@ -1,51 +1,56 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int main()
+int mystrcmp(const char* str1, const char* str2);
+int StringToNumber(char* str);
+char* NumberToString(int number);
+char* Uppercase(char* str);
+char* Lowercase(char* str);
+char* mystrrev(char* str);
 
-{
-
-	const int time = 86400;
-
-	int a;
-
-	cout << "Enter the time in seconds elapsed since the beginning of the day" << endl;
-
-	cin >> a;
-
-	int hh = a % time / 3600;
-	int mm = a / 60 % 60;
-	int ss = a % 60;
-
-	int endhh, endmm, endss;
-
-	int tmp = hh * 3600 + mm * 60 + ss;
-
-	tmp = time - tmp;
-	endhh = tmp / 3600;
-	endmm = tmp / 60 - endhh * 60;
-	endss = tmp - endmm * 60 - endhh * 3600;
-
-	cout << "Now is: " << hh << " hh: " << mm << " mm: " << ss << " ss" << endl;
-
-	cout << "before midnight: " << endhh << " hh: " << endmm << " mm: " << endss << " ss" << endl << endl;
-
-	int mainNumber = 1234;
-
-	cout << "Given an integer: " << mainNumber << endl;
-	cout << "Reversed number: ";
-	cout << mainNumber % 10;
-	mainNumber /= 10;
-	cout << mainNumber % 10;
-	mainNumber /= 10;
-	cout << mainNumber % 10;
-	mainNumber /= 10;
-	cout << mainNumber % 10;
-	mainNumber /= 10;
-
-	cout << endl << endl;
-
-	return 0;
-
+int main() {
+    // Testing mystrcmp function
+    char str1[] = "hello";
+    char str2[] = "world";
+    int result = mystrcmp(str1, str2);
+    if (result == 0) {
+        cout << "Strings are equal" << endl;
+    } else if (result > 0) {
+        cout << "String 1 is greater than String 2" << endl;
+    } else {
+        cout << "String 1 is less than String 2" << endl;
+    }
+    
+    // Testing StringToNumber function
+    char numStr[] = "123";
+    int num = StringToNumber(numStr);
+    cout << "Number: " << num << endl;
+    
+    // Testing NumberToString function
+    int number = 456;
+    char* str = NumberToString(number);
+    cout << "String: " << str << endl;
+    delete[] str;
+    
+    // Testing Uppercase function
+    char lowercase[] = "hello";
+    char* uppercase = Uppercase(lowercase);
+    cout << "Uppercase: " << uppercase << endl;
+    delete[] uppercase;
+    
+    // Testing Lowercase function
+    char uppercase2[] = "WORLD";
+    char* lowercase2 = Lowercase(uppercase2);
+    cout << "Lowercase: " << lowercase2 << endl;
+    delete[] lowercase2;
+    
+    // Testing mystrrev function
+    char original[] = "reverse";
+    char* reversed = mystrrev(original);
+    cout << "Reversed: " << reversed << endl;
+    delete[] reversed;
+    
+    return 0;
 }
