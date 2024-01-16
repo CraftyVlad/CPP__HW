cf656aee8db309e46d78010d76a27326c7a26375
CraftyVlad <107984683+CraftyVlad@users.noreply.github.com>
2024-01-16 16:06:27 +0200

    Update Task1.cpp

--- Task1&2.cpp
+++ Task1&2.cpp
@@ -11,7 +11,6 @@ char* Lowercase(char* str);
 char* mystrrev(char* str);
 
 int main() {
-    // Testing mystrcmp function
     char str1[] = "hello";
     char str2[] = "world";
     int result = mystrcmp(str1, str2);
@@ -23,30 +22,25 @@ int main() {
         cout << "String 1 is less than String 2" << endl;
     }
     
-    // Testing StringToNumber function
     char numStr[] = "123";
     int num = StringToNumber(numStr);
     cout << "Number: " << num << endl;
     
-    // Testing NumberToString function
     int number = 456;
     char* str = NumberToString(number);
     cout << "String: " << str << endl;
     delete[] str;
     
-    // Testing Uppercase function
     char lowercase[] = "hello";
     char* uppercase = Uppercase(lowercase);
     cout << "Uppercase: " << uppercase << endl;
     delete[] uppercase;
-    
-    // Testing Lowercase function
+
     char uppercase2[] = "WORLD";
     char* lowercase2 = Lowercase(uppercase2);
     cout << "Lowercase: " << lowercase2 << endl;
     delete[] lowercase2;
     
-    // Testing mystrrev function
     char original[] = "reverse";
     char* reversed = mystrrev(original);
     cout << "Reversed: " << reversed << endl;
